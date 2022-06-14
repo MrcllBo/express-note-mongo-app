0075cfbdbb599b89e205a15f40eef0b8760183e7
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-06-14 16:39:07 +0200

    Add note entity

---
+++ models/note.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const NoteSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  public: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    required: [true, 'Created date is required'],
+  },
+  updated: {type: Date, default: Date.now()},
+}, {versionKey: '_v1'});
+
+const Note = mongoose.model('Note', NoteSchema);
+
+export default Note;
